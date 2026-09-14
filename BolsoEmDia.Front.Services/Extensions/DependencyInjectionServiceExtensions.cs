b15dde67ab6273a67cd5226f3f4c6b79a537c81d
andreo210 <98775473+andreo210@users.noreply.github.com>
2026-09-14 16:13:35 -0300

    orçamento

--- BolsoEmDia.Front.Services/Extensions/DependencyInjectionServiceExtensions.cs
+++ BolsoEmDia.Front.Services/Extensions/DependencyInjectionServiceExtensions.cs
@@ -3,6 +3,7 @@ using BolsoEmDia.Front.Services.Servicos;
 using BolsoEmDia.Front.Services.Servicos.Autenticacao;
 using BolsoEmDia.Front.Services.Servicos.Categoria;
 using BolsoEmDia.Front.Services.Servicos.Conta;
+using BolsoEmDia.Front.Services.Servicos.Orcamento;
 using BolsoEmDia.Front.Services.Servicos.Transacao;
 using BolsoEmDia.Front.Services.Servicos.Transferencia;
 using BolsoEmDia.Front.Services.Utils.Notificacao;
@@ -49,6 +50,7 @@ namespace BolsoEmDia.Front.Services.Extensions
             services.AddScoped<ITransacaoService, TransacaoService>();
             services.AddScoped<ICategoriaService, CategoriaService>();
             services.AddScoped<ITransferenciaService, TransferenciaService>();
+            services.AddScoped<IOrcamentoService, OrcamentoService>();
 
             return services;
         }
